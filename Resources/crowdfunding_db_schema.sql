60d4939728332c8a9683a5424aa99e0c95426c11
MpatiM <159741444+MpatiM@users.noreply.github.com>
2024-05-13 19:39:00 -0400

    Add files via upload

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,8 +1,10 @@
+--Drop Tables if Exists--
 DROP TABLE "category" CASCADE;
 DROP TABLE "subcategory" CASCADE;
 DROP TABLE "contacts" CASCADE;
 DROP TABLE "campaign" CASCADE;
 
+--Create Table "category"--
 CREATE TABLE "category" (
     "category_id" VARCHAR(5)   NOT NULL,
     "category" VARCHAR(50)   NOT NULL,
@@ -13,7 +15,9 @@ CREATE TABLE "category" (
         "category"
     )
 );
+--Import category.csv data into table--
 
+--Create Table "subcategory"
 CREATE TABLE "subcategory" (
     "subcategory_id" VARCHAR(10)   NOT NULL,
     "subcategory" VARCHAR(50)   NOT NULL,
@@ -24,7 +28,9 @@ CREATE TABLE "subcategory" (
         "subcategory"
     )
 );
+--Import subcategory.csv data into table--
 
+--Create Table "contacts"
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
     "first_name" VARCHAR(15)   NOT NULL,
@@ -34,7 +40,9 @@ CREATE TABLE "contacts" (
         "contact_id"
      )
 );
+--Import contacts.csv data into table--
 
+--Create Table "campaign"
 CREATE TABLE "campaign" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
@@ -55,6 +63,7 @@ CREATE TABLE "campaign" (
      )
 );
 
+--Alter the "campaign" table to indicate the foreign keys--
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
 
@@ -63,8 +72,5 @@ REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
---Check Table imports--
-SELECT * FROM "category" ;
-SELECT * FROM "subcategory";
-SELECT * FROM "contacts";
-SELECT * FROM "campaign";
+
+--Import campaign.csv data into table--
